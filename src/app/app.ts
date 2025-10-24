8cab10c955c71c10d87629c8d9e83cd47fb27ba6
Enrico Nascimento SIlva <enriconascimento9@gmail.com>
2025-10-24 15:35:21 -0300

    Implementar conexao com API e exibir json no console

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
+
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+
+export class AppComponent implements OnInit {
   protected readonly title = signal('CRUD-Clientes');
+  http = inject(HttpClient);
+  apiUrl = 'http://localhost:3000/clientes';
+
+  ngOnInit() {
+    this.listarClientes();
+  }
+
+
+  listarClientes() {
+    this.http.get(this.apiUrl).subscribe(
+      (data) => {
+        console.log('Clientes:', data);
+      },
+      (error) => {
+        console.error('Erro ao listar clientes:', error);
+      }
+    );
+  }
 }
